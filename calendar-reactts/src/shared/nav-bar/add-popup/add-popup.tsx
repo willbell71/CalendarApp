16b1add9c9a1f726c01032230dbfc0c01f79e96a
William Bell <willbee71@googlemail.com>
2019-10-18 08:22:11 +0100

    Feature: added pop up mark up and styles

--- calendar-reactts/src/shared/nav-bar/add-popup/add-popup.tsx
+++ calendar-reactts/src/shared/nav-bar/add-popup/add-popup.tsx
@@ -12,7 +12,10 @@ export class AddPopup extends React.Component {
    */
   public render(): JSX.Element {
     return (
-      <div/>
+      <section className="add-popup">
+        <label htmlFor="add-popup-input"><h3 className="add-popup__title">Create Quick Event</h3></label>
+        <input id="add-popup-input" className="add-popup__input" type="text" placeholder="Film at 7pm on Friday"/>
+      </section>
     );
   }
 }
